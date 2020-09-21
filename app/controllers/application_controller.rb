0078cfbd4f14d6a6775dea89b32bd8783ee74be8
ajkelso <ajkelso@gmail.com>
2020-09-21 16:54:04 -0400

    create #login, #create_user, #logged_in?, #current_user Helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,31 @@ class ApplicationController < Sinatra::Base
         "Hello World!"
     end
 
+    helpers do
+        def logged_in?
+            !!session[:user_id]
+        end
+
+        def current_user
+            User.find(session[:user_id])
+        end
+
+        def login(user_id)
+            session[:user_id] = user_id
+        end
+
+        def create_user(params)
+            @user = User.new(name: params[:name], username: params[:username], email: params[:email], password: params[:password])
+            if User.all.find {|u| u.username == @user.username}
+                redirect '/signup'
+            elsif @user.save
+                login(@user.id)
+                redirect '/welcome'
+            else
+                redirect '/signup'
+            end
+        end
+
+    end
+
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,9 +6,8 @@ class UserController < ApplicationController
    
     post '/signup' do
         binding.pry
+        create_user(params)
         @user = User.new(name: params[:name], username: params[:username], email: params[:email], password: params[:password])
-
-        
     end
 
 end
